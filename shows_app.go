8d440a892355231903d8918d8b47123bbc91d299
joshkestenberg <joshkestenberg@gmail.com>
2017-04-05 16:42:11 -0400

    mans making succesful posts via get request query string!

--- shows_app.go
+++ shows_app.go
@@ -44,6 +44,8 @@ func showsArtistYearHandler(w http.ResponseWriter, r *http.Request){
   }
 }
 
+// version 1 = post request from form
+
 func newHandler(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w, "<h1>New Show</h1><form action='/shows' method='POST'><input type='text' name='artist' placeholder='artist'><br>year<input type='number' name='year' min='1950' max='2020'><br>month<input type='number' name='month' min='1' max='12'><br>day<input type='number' name='day' min='1' max='31'><br><input type='text' name='city' placeholder='city'><br><input type='text' name='venue' placeholder='venue'><br><input type='submit'></form>")
 }
@@ -79,3 +81,41 @@ func main() {
 
   log.Fatal(http.ListenAndServe(":8000", r))
 }
+
+//version 2 = get request from form
+
+// func newHandler(w http.ResponseWriter, r *http.Request){
+//   fmt.Fprintf(w, "<h1>New Show</h1><form action='/shows' method='POST'><input type='text' name='artist' placeholder='artist'><br>year<input type='number' name='year' min='1950' max='2020'><br>month<input type='number' name='month' min='1' max='12'><br>day<input type='number' name='day' min='1' max='31'><br><input type='text' name='city' placeholder='city'><br><input type='text' name='venue' placeholder='venue'><br><input type='submit'></form>")
+// }
+//
+// func saveHandler(w http.ResponseWriter, r *http.Request){
+//   r.ParseForm()
+//   fmt.Println()
+//
+//   var s Show
+//   s.artist = r.Form["artist"][0]
+//   s.year, _ = strconv.Atoi(r.Form["year"][0])
+//   s.month, _ = strconv.Atoi(r.Form["month"][0])
+//   s.day, _ = strconv.Atoi(r.Form["day"][0])
+//   s.city = r.Form["city"][0]
+//   s.venue = r.Form["venue"][0]
+//
+//   shows = append(shows, s)
+//
+//   http.Redirect(w, r, "/shows", 301)
+// }
+//
+// func main() {
+//   r := mux.NewRouter()
+//   shows = append(shows, Show{artist: "Phish", year: 2017, month: 4, day: 20, city: "Guelph", venue: "Up Your Friggin Dick"})
+//   shows = append(shows, Show{artist: "The Disco Biscuits", year: 2017, month: 5, day: 21, city: "Guelph", venue: "Up Your Friggin Balls"})
+//   shows = append(shows, Show{artist: "Creed", year: 2017, month: 6, day: 22, city: "Guelph", venue: "Up Your Friggin Ass"})
+//
+//   r.HandleFunc("/shows", showsHandler).Methods("GET")
+//   r.HandleFunc("/shows/{artist}", showsArtistHandler).Methods("GET")
+//   r.HandleFunc("/shows/{artist}/{year}", showsArtistYearHandler).Methods("GET")
+//   r.HandleFunc("/new", newHandler).Methods("GET")
+//   r.HandleFunc("/shows", saveHandler).Methods("POST")
+//
+//   log.Fatal(http.ListenAndServe(":8000", r))
+// }
